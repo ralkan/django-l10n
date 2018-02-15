31eab3e22a9e9f7ddfd85d7cfebd22c671636571
Resul Alkan <me@resulalkan.com>
2018-02-15 10:41:02 +0300

    Added support for lazy translations

--- djl10n/utils.py
+++ djl10n/utils.py
@@ -1,4 +1,6 @@
 from django.utils.translation import get_language
+from django.utils.functional import lazy
+from django.utils import six
 
 from .helpers import translate_by_key
 
@@ -6,3 +8,6 @@ from .helpers import translate_by_key
 def localize(key, default=None, **kwargs):
     lang_code = get_language()
     return translate_by_key(lang_code, key, default, **kwargs)
+
+
+localize_lazy = lazy(localize, six.text_type)
